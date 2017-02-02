78ce9ee7c291123ae432202f4665603cb962fe2e
Paul An <anpaul0615@gmail.com>
2017-02-02 11:26:16 +0900

    (5w, ch06~ch07) Blog
     * apply bootstrap layout
     * apply tag-cloud

--- gdg-busan-django/mysite/blog/views.py
+++ gdg-busan-django/mysite/blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, TodayArchiveView
+from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic.dates import ArchiveIndexView
+from django.views.generic.dates import YearArchiveView, MonthArchiveView, DayArchiveView, TodayArchiveView
 
-from blog.models import Post
+
+from .models import Post
+from tagging.models import Tag, TaggedItem
+from tagging.views import TaggedObjectList
 
 # Create your views here.
 #-- ListView
@@ -20,20 +24,34 @@ class PostDV(DetailView) :
 class PostAV(ArchiveIndexView) :
   model = Post
   date_field = 'modify_date'
+  month_format = '%m'
 
 class PostYAV(YearArchiveView) :
   model = Post
   date_field = 'modify_date'
   make_object_list =True
+  month_format = '%m'
 
 class PostMAV(MonthArchiveView) :
   model = Post
   date_field = 'modify_date'
+  month_format = '%m'
 
 class PostDAV(DayArchiveView) :
   model = Post
   date_field = 'modify_date'
+  month_format = '%m'
 
 class PostTAV(TodayArchiveView) :
   model = Post
   date_field = 'modify_date'
+  month_format = '%m'
+
+
+#--- TemplateView
+class TagTV(TemplateView) :
+  template_name = 'tagging/tagging_cloud.html'
+
+class PostTOL(TaggedObjectList):
+  model = Post
+  template_name = 'tagging/tagging_post_list.html'

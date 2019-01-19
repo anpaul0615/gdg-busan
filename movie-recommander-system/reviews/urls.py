fe7ce612548cf4becb1c9d2768656ff2a0856d5e
PAUL AN <anpaul0615@gmail.com>
2019-01-19 16:40:44 +0900

    Implement Registration/Login/Logout

--- movie-recommander-system/reviews/urls.py
+++ movie-recommander-system/reviews/urls.py
@@ -5,4 +5,7 @@ app_name = 'reviews'
 
 urlpatterns = [
     path('', views.index, name='Index'),
+    path('registration', views.register, name='Registration'),
+    path('login', views.user_login, name='Login'),
+    path('logout', views.user_logout, name='Logout'),
 ]
